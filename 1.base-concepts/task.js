18ba0e58ab66e511363d5e93fd22ff9e8f7a43db
andy <lupolenkoandre@gmail.com>
2026-01-13 13:12:22 +0300

    task one add

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
-"use strict"
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  const discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant < 0) {
+    return [];
+  }
+
+  if (discriminant === 0) {
+    const root = -b / (2 * a);
+    return [root];
+  }
+
+  const sqrtDiscriminant = Math.sqrt(discriminant);
+  const root1 = (-b + sqrtDiscriminant) / (2 * a);
+  const root2 = (-b - sqrtDiscriminant) / (2 * a);
+
+  return [root1, root2];
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  const monthPercent = percent / 100 / 12;
+  const creditBody = amount - contribution;
+  const monthPayment =
+    creditBody * (monthPercent + monthPercent / (Math.pow(1 + monthPercent, countMonths) - 1));
+  return +(monthPayment * countMonths).toFixed(2);
 }
