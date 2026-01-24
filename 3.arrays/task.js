7321f470f495c2c6b3f24cfc8d489c2e1c11876e
andy <lupolenkoandre@gmail.com>
2026-01-24 13:30:52 +0300

    Complete task 3 arrays implementation

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+  if (users.length === 0) {
+    return 0;
+  }
+
+  const filteredUsers = users.filter(user => user.gender === gender);
   
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+  const averageAge = totalAge / filteredUsers.length;
+
+  return averageAge;
 }
